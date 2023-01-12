28aa9de937df747b19180319d860171a0de40b3e
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-12 16:58:11 +0100

    added footer

--- portfolio/src/app/contact/contact.component.ts
+++ portfolio/src/app/contact/contact.component.ts
@@ -12,4 +12,9 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
 }
