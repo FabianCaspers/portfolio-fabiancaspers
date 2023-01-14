34f70d024f7c03a533676d9937f030e4f07d2f2f
FabianCaspers <119706742+FabianCaspers@users.noreply.github.com>
2023-01-14 13:17:04 +0100

    make responsive

--- portfolio/src/app/header/header.component.ts
+++ portfolio/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('mobileMenuButton') mobileMenuButton!: ElementRef;
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
 
   constructor() { }
+
+  openAndCloseMenu(){
+    this.mobileMenuButton.nativeElement.classList.toggle('active');
+    this.mobileMenu.nativeElement.classList.toggle('show');
+  }
 
   ngOnInit(): void {
   }
